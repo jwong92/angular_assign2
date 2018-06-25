09277d16e773beb64539d096efcf866d1eba5493
tyzia <tyziaa@gmail.com>
2018-06-25 18:21:43 -0400

    created route for my component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes, Router } from '@angular/router';
 
-import {HomepageComponent} from './homepage/homepage.component'
+import {HomepageComponent} from './homepage/homepage.component';
 import {DetailsComponent} from './details/details.component';
 import {ResultsComponent} from './results/results.component';
+import { WeatherlistComponent } from './weatherlist/weatherlist.component';
 
 const routes: Routes = [
   {path: '', component: HomepageComponent},
   {path: 'details/:id', component: DetailsComponent},
-  {path: 'results/:name', component: ResultsComponent}
+  {path: 'results/:name', component: ResultsComponent},
+    { path: 'andrei', component: WeatherlistComponent },
+    { path: 'andrei/:id', component: WeatherlistComponent }
 ]
 
 @NgModule({
